8946476a1e8f1ea006d40f4eaf9ad78cdc5019cb
Ruin21 <118554907+Ruin2121@users.noreply.github.com>
2023-04-08 16:41:33 -0400

    finished setting up setup.py and added .gitignore

--- setup.py
+++ setup.py
@@ -3,4 +3,28 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
+setup(
+    name="technical-analysis",
+    version="2023.4.8.1",
+    description="A simple, generic Python technical analysis library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ruin2121/technical-analysis",
+    author="Ruin2121",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        ],
+    keywords="technical analysis",
+    packages=find_packages("technical_analysis"),
+    python_requires=">=3.9, <4",
+    # install_requires=[],
+    )
