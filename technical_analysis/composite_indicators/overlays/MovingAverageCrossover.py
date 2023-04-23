cb9452bf5aec6b09536f265325bdf6dd74da3f5f
Ruin21 <118554907+Ruin2121@users.noreply.github.com>
2023-04-22 20:03:49 -0400

    Final fixes (hopefully)

--- technical_analysis/composite_indicators/overlays/MovingAverageCrossover.py
+++ technical_analysis/composite_indicators/overlays/MovingAverageCrossover.py
@@ -27,12 +27,6 @@ class MovingAverageCrossover(BaseIndicatorClass):
         ma1_values = ma1.to_np_array()
         ma2_values = ma2.to_np_array()
 
-        # Detects when ma1 crosses above ma2
-        ma1_cross_up = np.where(ma1_values > ma2_values, 1, 0)
-
-        # Detects when ma1 crosses below ma2
-        ma1_cross_down = np.where(ma1_values < ma2_values, 2, 0)
-
         # Initialize an array to store the results
         results = np.zeros_like(self.__numpy_data)
 
