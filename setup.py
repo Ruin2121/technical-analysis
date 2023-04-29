17949801f10e80781c0a74bfeb86839c211fa167
Ruin2121 <118554907+Ruin2121@users.noreply.github.com>
2023-04-29 18:35:40 +0000

    test commit from replit.com

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="technical-analysis",
-    version="2023.4.29.1",
+    version="2023.4.29.2",
     description="A simple, generic Python technical analysis library",
     long_description=long_description,
     long_description_content_type="text/markdown",
